ab75ae1cc860f41d8d60f98099fe8e73b4065207
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2025-08-20 20:09:53 +0530

    add dark mode

--- admin/components/AppSidebar.tsx
+++ admin/components/AppSidebar.tsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarSeparator,
+} from "@/components/ui/sidebar";
+import { Calendar, ChevronUp, Home, Inbox, Search, Settings, User } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+
+const items = [
+  {
+    title: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
 
 const AppSidebar = () => {
   return (
-    <div>AppSidebar</div>
-  )
-}
+      <Sidebar collapsible="icon">
+        <SidebarHeader className="py-4">
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild>
+                <Link href="/" className="flex items-center">
+                  <Image src="/globe.svg" height={20} width={20} alt="logo" />
+                  <p>Vendo</p>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarHeader>
+        <SidebarSeparator/>
 
-export default AppSidebar
+        <SidebarContent>
+          <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        </SidebarContent>
+
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <SidebarMenuButton>
+                        <User/> John Doe <ChevronUp className="ml-auto"/>
+                    </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuItem>Account</DropdownMenuItem>
+                  <DropdownMenuItem>Setting</DropdownMenuItem>
+                  <DropdownMenuItem>SignOut</DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      </Sidebar>
+  );
+};
+
+export default AppSidebar;
--- admin/components/Navbar.tsx
+++ admin/components/Navbar.tsx
@@ -1,22 +1,65 @@
-import Link from 'next/link'
-import React from 'react'
-import {Moon} from 'lucide-react'
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
+"use client"
+import Link from "next/link";
+import React from "react";
+import { LogOut, Moon,Sun, Settings, User } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "next-themes"
+import { Button } from "./ui/button";
+import { SidebarTrigger } from "./ui/sidebar";
 
 const Navbar = () => {
+    const {setTheme}= useTheme(); 
   return (
-    <div className='w-full bg-amber-400 flex items-center justify-between p-4'>
-        CollapseSidebar
-        <div className='flex items-center gap-8'>
-            <Link href='/'>Dashboard</Link>
-            <Moon/>
+    <div className="w-full flex items-center justify-between py-4 px-8">
+      <SidebarTrigger/>
+      <div className="flex items-center gap-8">
+        <Link href="/">Dashboard</Link>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
             <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" />
-                <AvatarFallback>CN</AvatarFallback>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
             </Avatar>
-        </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={12}>
+            <DropdownMenuLabel>Profile</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem><User/> Profile</DropdownMenuItem>
+            <DropdownMenuItem><Settings/> Settings</DropdownMenuItem>
+            <DropdownMenuItem variant="destructive"><LogOut/> Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
